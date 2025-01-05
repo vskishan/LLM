7df765fc6bed060bcde994c2c2692ff7e717878a
Kishan <vskishanreddy999@gmail.com>
2025-01-05 20:51:07 +0530

    Using a templated system message

---
+++ langchain/llm/src/main/java/com/example/llm/model/UserInput.java
@@ -0,0 +1,16 @@
+package com.example.llm.model;
+
+public class UserInput {
+    
+    String message;
+
+    String information;
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String getInformation() {
+        return this.information;
+    }
+}
