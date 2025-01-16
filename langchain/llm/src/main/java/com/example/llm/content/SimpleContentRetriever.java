5edace7a9e5f07a9c71113506249d4439cbf953a
Kishan <vskishanreddy999@gmail.com>
2025-01-16 22:53:03 +0530

    Simple RAG using in-memory embedding store

--- langchain/llm/src/main/java/com/example/llm/content/SimpleContentRetriever.java
+++ langchain/llm/src/main/java/com/example/llm/content/SimpleContentRetriever.java
@@ -1,12 +1,14 @@
 package com.example.llm.content;
 
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.example.llm.parser.JsonDocumentParser;
-
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.UrlDocumentLoader;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
@@ -16,18 +18,31 @@ import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 @Configuration
 public class SimpleContentRetriever {
 
+    @Value("${langchain4j.open-ai.chat-model.api-key}")
+    private String apiKey;
+
     @Bean(name = "SimpleContentRetriever")
     public ContentRetriever getContent() {
 
-        JsonDocumentParser jsonDocumentParser = new JsonDocumentParser();
-
-        Document document = UrlDocumentLoader.load("https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json", jsonDocumentParser);
+        Document document = UrlDocumentLoader.load("https://gist.githubusercontent.com/emarco177/0d6a3f93dd06634d95e46a2782ed7490/raw/78233eb934aa9850b689471a604465b188e761a0/eden-marco.json", new TextDocumentParser());
 
         InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
 
-        EmbeddingStoreIngestor.ingest(document, embeddingStore);
+        //EmbeddingModel embeddingModel = OpenAiEmbeddingModel.builder().apiKey(apiKey).build();
 
-        return EmbeddingStoreContentRetriever.from(embeddingStore);
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+                .embeddingModel(embeddingModel)
+                .embeddingStore(embeddingStore)
+                .build();
+
+        ingestor.ingest(document);
+
+        return EmbeddingStoreContentRetriever.builder()
+        .embeddingStore(embeddingStore)
+        .embeddingModel(embeddingModel)
+        .build();
 
     }
     
